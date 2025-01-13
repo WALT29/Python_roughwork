b79aab434b1b104b7d56ea55443678d9de5d01e1
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2025-01-13 07:51:52 +0300

    cont

---
+++ main.py
@@ -0,0 +1,44 @@
+#   TRY AND EXCEPT STATEMENT
+"""
+- try/except statements allows us to intercept exceptions
+
+- try - lets you test a block of code for errors
+- except - lets you handle the error
+- else  - lets you execute the block of code when there is no error
+- finally - lets you execute the block of code regardless of the error or not
+
+
+
+
+
+    EG:
+        def divide(num1,num2):
+            try:
+                division=num1/num2
+                print(division)
+            except:
+                print("An error occurred")
+
+            divide(1,0)
+        
+        - This results to an error whenever num2 is 0 or non-numerical value
+        
+
+
+
+
+
+
+
+
+"""
+
+
+def divide(num1,num2):
+    try:
+        division=num1/num2
+        print(division)
+    except:
+        print("An error occurred")
+        
+divide(1,0)
