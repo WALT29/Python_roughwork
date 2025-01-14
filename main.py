501caca1ea5cdf97432a45e8d96b105d465b1762
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2025-01-14 22:13:21 +0300

    Adding exceptions

--- main.py
+++ main.py
@@ -22,14 +22,6 @@
             divide(1,0)
         
         - This results to an error whenever num2 is 0 or non-numerical value
-        
-
-
-
-
-
-
-
 
 """
 
@@ -42,3 +34,11 @@ def divide(num1,num2):
         print("An error occurred")
         
 divide(1,0)
+
+# Adding execeptions 
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined")
+except:
+    print("Something else")
